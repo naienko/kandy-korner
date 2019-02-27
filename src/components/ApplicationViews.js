2e2f2df859b1fcdca1f5918aebd204793cb7e340
Panya Farnette <naienko@gmail.com>
2019-02-27 13:48:07 -0600

    fetches data from database

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,12 +6,34 @@ import EmployeeList from './EmployeeList';
 
 export default class ApplicationView extends Component {
 
-    state = {
-        candies: this.SpecificCandies,
-        employees: this.EmployeesFromAPI,
-        locations: this.LocationsFromAPI,
-        types: this.CandyTypes
+    componentDidMount() {
+        const newState = {};
+
+        fetch("http://localhost:8081/locations/")
+            .then(results => results.json())
+            .then(locations => newState.locations = locations)
+
+            .then(() => fetch("http://localhost:8081/employees/"))
+            .then(results => results.json())
+            .then(employees => newState.employees = employees)
+
+            .then(() => fetch("http://localhost:8081/candies/"))
+            .then(results => results.json())
+            .then(candies => newState.candies = candies)
+
+            .then(() => fetch("http://localhost:8081/types/"))
+            .then(results => results.json())
+            .then(types => newState.types = types)
+// NOTE: you HAVE to do setState or your new data fetched just now is never going to show up in the app
+            .then(() => this.setState(newState))
     }
+
+    state = {
+        candies: [],
+        employees: [],
+        locations: [],
+        types: []
+    };
 
     render() {
         return (
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 export default class NavBar extends Component {
     render() {
         return (
-            <nav>
+            <nav className="navbar navbar-light fixed-top light-blue flex-md-nowrap p-0 shadow">
                 <li className="nav-item">
                     <Link className="nav-link" to="/stores">Stores</Link>
                 </li>
