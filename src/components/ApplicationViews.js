9ea10db572227f8cccdb9e865560070447208906
Panya Farnette <naienko@gmail.com>
2019-03-04 15:54:52 -0600

    merge branch 'master' into dynamic-routing completed

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { Route } from "react-router-dom";
-
-=======
 import { Route, Redirect } from "react-router-dom";
->>>>>>> master
+
 import LocationList from './location/LocationList';
 import CandyList from './candy/CandyList';
 import EmployeeList from './employee/EmployeeList';
 
 import SearchResults from './search/SearchResults';
+import Login from "./authentication/Login";
 
 import LocationManager from "../modules/api/LocationManager";
 import EmployeeManager from "../modules/api/EmployeeManager";
 import CandyManager from "../modules/api/CandyManager";
 import TypeManager from "../modules/api/TypeManager";
+
 import EmployeeForm from "./employee/EmployeeForm";
 import CandyForm from "./candy/CandyForm";
-import Login from "./authentication/Login";
 
 import EmployeeDetail from "./employee/EmployeeDetail";
 import CandyDetail from './candy/CandyDetail';
@@ -81,35 +78,6 @@ export default class ApplicationView extends Component {
     render() {
         return (
             <React.Fragment>
-<<<<<<< HEAD
-                <Route exact path="/locations" render={() => {
-                    return <LocationList locations={this.state.locations}
-                        employees={this.state.employees} />
-                }} />
-                <Route path="/locations/:locationId(\d+)" render={(props) => {
-                    return <LocationDetail {...props} locations={this.state.locations}
-                        employees={this.state.employees} />
-                }} />
-
-                <Route exact path="/employees" render={() => {
-                    return <EmployeeList employees={this.state.employees} />
-                }} />
-                <Route path="/employees/:employeeId(\d+)" render={(props) => {
-                    return <EmployeeDetail {...props} employees={this.state.employees} />
-                }} />
-
-                <Route exact path="/candies" render={() => {
-                    return <CandyList candies={this.state.candies}
-                        types={this.state.types} 
-                        deleteCandy={this.deleteCandy} />
-                }}
-                />
-                <Route path="/candies/:candyId(\d+)" render={(props) => {
-                    return <CandyDetail {...props} 
-                        types={this.state.types} 
-                        candies={this.state.candies} />
-=======
-
                 <Route path="/login" component={Login} />
 
                 <Route exact path="/locations" render={(props) => {
@@ -120,8 +88,16 @@ export default class ApplicationView extends Component {
                     } else {
                         return <Redirect to="/login" />
                     }
-                    
                 }} />
+                <Route path="/locations/:locationId(\d+)" render={(props) => {
+                    if (this.isAuthenticated()) {
+                        return <LocationDetail {...props} locations={this.state.locations}
+                            employees={this.state.employees} />
+                    } else {
+                        return <Redirect to="/login" />
+                    }
+                }} />
+
                 <Route exact path="/employees" render={(props) => {
                     if (this.isAuthenticated()) {
                         return <EmployeeList {...props} employees={this.state.employees} />
@@ -134,6 +110,13 @@ export default class ApplicationView extends Component {
                         return <EmployeeForm {...props} 
                             addEmployee={this.addEmployee} 
                             locations={this.state.locations} />
+                    } else {
+                        return <Redirect to="/login" />
+                    }
+                }} />
+                <Route path="/employees/:employeeId(\d+)" render={(props) => {
+                    if (this.isAuthenticated()) {
+                        return <EmployeeDetail {...props} employees={this.state.employees} />
                     } else {
                         return <Redirect to="/login" />
                     }
@@ -157,7 +140,15 @@ export default class ApplicationView extends Component {
                     } else {
                         return <Redirect to="/login" />
                     }
->>>>>>> master
+                }} />
+                <Route path="/candies/:candyId(\d+)" render={(props) => {
+                    if (this.isAuthenticated()) {
+                        return <CandyDetail {...props} 
+                            types={this.state.types} 
+                            candies={this.state.candies} />
+                    } else {
+                        return <Redirect to="/login" />
+                    }
                 }} />
 
                 <Route path="/search" render={(props) => {
--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import EmployeeList from "../employee/EmployeeList";
 
 export default class LocationDetail extends Component {
+    isLocation = true;
+
     render() {
         /*
             Using the route parameter, find the animal that the
@@ -22,10 +24,10 @@ export default class LocationDetail extends Component {
                                 <React.Fragment>
                                     <p>Employees</p>
                                     <section className="employees-local">
-                                        <EmployeeList employees={this.props.employees
+                                        <EmployeeList isLocation={this.isLocation} employees={this.props.employees
                                             .filter( employee => employee.storeId === location.id && employee.position === "manager" )
                                         }/>
-                                        <EmployeeList employees={this.props.employees
+                                        <EmployeeList isLocation={this.isLocation} employees={this.props.employees
                                             .filter( employee => employee.storeId === location.id && employee.hasOwnProperty("position") !== true )
                                             .sort((a, b) => (a.last_name > b.last_name) ? 1 : -1)
                                         }/>
